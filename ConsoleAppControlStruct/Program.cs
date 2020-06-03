8966b9db8c76ccfca129436fab5a9bed3320f76d
sunyubo <yubo.sun@dbh.dynabook.com>
2020-06-03 17:32:23 +0800

    添加控制语句的例子

---
+++ ConsoleAppControlStruct/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using static System.Console;
+
+namespace ConsoleAppControlStruct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestSwitch();
+            WriteLine("Type in a string");
+            string input;
+            input = ReadLine();
+
+            if (input=="")
+            {
+                WriteLine("you typed in an empty string.");
+            }
+            else if (input.Length<5)
+            {
+                WriteLine("The string had less than 5 characters.");
+            }
+            else
+            {
+                WriteLine($"The string was {input}");
+            }
+
+            WriteLine("Press any key to continue...");
+            ReadKey();
+        }
+
+        static void TestSwitch()
+        {
+            string country;
+            WriteLine("Type in a city");
+            country = ReadLine();
+            switch (country)
+            {
+                case "Hangzhou":
+                    MessageBox.Show("Hangzhou");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
