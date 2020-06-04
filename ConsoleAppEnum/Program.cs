639298562f90ca3f5212a7dd8cf91a5ad5e25634
sunyubo <yubo.sun@dbh.dynabook.com>
2020-06-04 16:38:25 +0800

    添加枚举类型例子

---
+++ ConsoleAppEnum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleAppEnum
+{
+    class Program
+    {
+        /// <summary>
+        /// 一天的阶段
+        /// </summary>
+        public enum TimeOfDay
+        {
+            Morning,
+            Afternoon,
+            Evening
+        }
+
+        static void Main(string[] args)
+        {
+            Greeting(TimeOfDay.Morning);
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// greet.
+        /// </summary>
+        /// <param name="morning"></param>
+        private static void Greeting(TimeOfDay morning)
+        {
+            Console.WriteLine("Good "+morning.ToString());
+        }
+    }
+}
