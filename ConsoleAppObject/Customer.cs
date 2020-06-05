b40023cf18b1fb71c42d4480dca6c9a8f166d128
sunyubo <yubo.sun@dbh.dynabook.com>
2020-06-05 11:48:48 +0800

    添加对象示例。

---
+++ ConsoleAppObject/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppObject
+{
+    class Customer
+    {
+        public string Name { get; set; }
+        public void Consume()
+        {
+            Console.WriteLine("consume!!");
+        }
+    }
+}
---
+++ ConsoleAppObject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer() { Name = "Ruan Yangyang" };
+            customer.Consume();
+            Console.ReadKey();
+        }
+    }
+}
