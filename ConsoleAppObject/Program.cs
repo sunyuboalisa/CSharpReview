0366591f087461f39455642247f5f0cf79e4f7bb
sunyubo <yubo.sun@dbh.dynabook.com>
2020-06-10 11:57:49 +0800

    添加类和结构的例子

--- ConsoleAppObject/Program.cs
+++ ConsoleAppObject/Program.cs
@@ -12,6 +12,8 @@ namespace ConsoleAppObject
         {
             Customer customer = new Customer() { Name = "Ruan Yangyang" };
             customer.Consume();
+            Square square = new Square() { SideLength = 5 };
+            Console.WriteLine(square.GetArea());
             Console.ReadKey();
         }
     }
---
+++ ConsoleAppObject/Square.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+struct Square
+{
+    public double SideLength { get; set; }
+
+    public double GetArea()
+    {
+        return Math.Pow(SideLength,2);
+    }
+}
